dd714864a1ccdacd59be3f8c49d4078782140a6e
ekramzafar <133132379+ekramzafar@users.noreply.github.com>
2026-04-29 11:08:49 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCv9C5J18Q78nUqwMiLOsujdD0wR08kFIU",
+  apiKey: "Agddhfhjf",
   authDomain: "smart-volunteer-system-62c51.firebaseapp.com",
   projectId: "smart-volunteer-system-62c51",
   storageBucket: "smart-volunteer-system-62c51.firebasestorage.app",
-  messagingSenderId: "349787506166",
-  appId: "1:349787506166:web:76117a09c8f12c6b9ab3d7",
-  measurementId: "G-MCQGX13Y1D"
+  messagingSenderId: "3fhhfjft",
+  appId: "1:3497875yuy166:web:76117fghhtjfjtjc8f12c6b9ab3d7",
+  measurementId: "G-MCQGgfdhdfX13Y1D"
 };
 
 // Initialize Firebase
